72ae99717b6b46bd17ba0e9ec26151d09b9d3a6d
Dev~Ratnawat <officesipl24@gmail.com>
2025-09-18 13:50:09 +0530

    Update frontend api url

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ connectDB();
 
 app.use(cors());
 app.use(cors({
-  origin: "https://notes-app-3-rdzk.onrender.com", //frontend Render URL
+  origin: [
+    "http://localhost:5173", // for Vite local dev
+    "http://localhost:3000", // for CRA local dev
+    "https://notes-app-3-rdzk.onrender.com" // frontend Render URL
+  ],
   methods: "GET,POST,PUT,DELETE",
   credentials: true
 }));
