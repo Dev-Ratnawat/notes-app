2aee41424c81f2305d17c3a11f8d1c67b00574a0
Dev-Ratnawat <officesipl24@gmail.com>
2025-09-19 08:31:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  process.env.FRONTEND_URL
+  "https://notes-app-2-t08q.onrender.com"
 ].filter(Boolean); // removes undefined
 
 app.use(cors({
